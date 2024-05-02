a6d017fd5243ba93784cedd80c8d0965e6827b60
lorrainelbs <you@example.com>
2024-05-02 17:51:21 -0300

    segundo commit

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,7 +8,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function(){
         const longText = 'Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste, Teste'
         
         cy.get('#firstName').type('Lorraine')
@@ -29,6 +29,24 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
         cy.get('.error').should('be.visible')
         
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
+        cy.get('#phone')
+          .type('abcdefjhij')
+          .should('have.value', '')
+
+    })
+
+    it('telefone se torna obrigatório, mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#firstName').type('Lorraine')
+        cy.get('#lastName').type('Lacerda')
+        cy.get('#email').type('lorrainelacerda@gmail.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste') 
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
     })
 
   })

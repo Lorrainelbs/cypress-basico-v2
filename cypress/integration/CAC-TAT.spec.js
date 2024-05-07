87b7d2e6d8987382768521e28b6bd8c00e171ab4
Lorrainelbs <lorrainelacerda@gmail.com>
2024-05-07 10:32:43 -0300

    seleciona arquivo

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -38,18 +38,45 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    it('telefone se torna obrigatório, mas não é preenchido antes do envio do formulário', function(){
+    it('exibe mensagem de erro quando o telefone se torna obrigatório, mas não é preenchido antes do envio do formulário', function(){
         cy.get('#firstName').type('Lorraine')
         cy.get('#lastName').type('Lacerda')
         cy.get('#email').type('lorrainelacerda@gmail.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('teste') 
         cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
 
-    //espaço para o código que fiz em casa -> enviar commit para o github
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName')
+          .type('Lorraine')
+          .should('have.value','Lorraine')
+          .clear()
+          .should('have.value', '')
+        cy.get('#lastName')
+          .type('Lacerda')
+          .should('have.value','Lacerda')
+          .clear()
+          .should('have.value', '')
+        cy.get('#email')
+          .type('lorrainelacerda@gmail.com')
+          .should('have.value','lorrainelacerda@gmail.com')
+          .clear()
+          .should('have.value', '')
+        cy.get('#email')
+          .type('1234567890')
+          .should('have.value','1234567890')
+          .clear()
+          .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
 
     it('envia o formulário com sucesso usando um comando customizado', function(){
         cy.fillMandatoryFieldsAndSubmit()
@@ -89,14 +116,52 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('marca cada tipo de atendimento', function(){
         cy.get('input[type="radio"]')
         .should('have.length', 3)
-        .each(function($radio){
+        .each(function($radio){ //o $ indica uma JQuery
             cy.wrap($radio).check()
             cy.wrap($radio).should('be.checked') //para cada iteração ele pega um elemento
         })
 
     })
+    // .uncheck() 
+    it('marca ambos checkboxes, depois desmarca o último', function(){
+        cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')
 
-  })
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function(){
+        cy.get('input[type="file"]')
+        .should('not.have.value') //para garantir que nao tem nenhum arquivo selecionado
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function(){
+        cy.get('input[type="file"]')
+        .should('not.have.value') 
+        .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    }) //simulo que estou arrastando um arquivo que está na pasta do meu pc até a plataforma
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+        cy.fixture('example.json').as('sampleFile') //criação do alias
+        cy.get('input[type="file"]')
+          .selectFile('@sampleFile') //para usar o alias, passo o @
+      
+    }) 
+
+})
+
+
+  
 
 
   

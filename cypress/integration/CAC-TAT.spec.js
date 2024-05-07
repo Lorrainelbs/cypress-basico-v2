89cafd13a8ad0c9752802f708b72f64e4e32e57e
lorrainelbs <you@example.com>
2024-05-07 17:47:10 -0300

    ajustes json

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -158,6 +158,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       
     }) 
 
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a')
+          .should('have.attr', 'target', '_blank') 
+      
+    }) 
+
+    it('acessa a página de política de privacidade removendo o target e então clicando no link', function(){
+        cy.get('#privacy a')
+          .invoke('removeAttr', 'target')
+          .click()
+      
+          cy.contains('Talking About Testing').should('be.visible')
+    }) 
+
 })
 
 
